10d3ba79f8f0bebcb0f0b1c7b098d89bdca0eba9
Cloneable <cloneable@163.com>
2020-12-02 22:17:38 +0800

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201202122149) do
+ActiveRecord::Schema.define(version: 20201202131433) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false

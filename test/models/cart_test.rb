9d7a8a70a67f8860055a4fedf3765172949bb6ef
Cloneable <cloneable@163.com>
2020-12-02 22:15:35 +0800

    Creating a smart cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should build new line item when not same line item" do
+    cart = Cart.new
+    current_item = cart.add_product(products(:ruby).id)
+    assert !current_item.nil?
+    assert_equal 1, current_item.quantity
+  end
+
+  test "should increase quantity when there's same line item" do
+    cart = Cart.new
+    cart.add_product(products(:ruby).id)
+    cart.save!
+    current_item = cart.add_product(products(:ruby).id)
+    current_item.save
+    assert_equal 1, Cart.find(cart.id).line_items.count
+    assert_equal 2, current_item.quantity
+  end
 end

3c6c62833edb7cd78c4ef6e3186341507c471ea4
Cloneable <1573652017@qq.com>
2020-12-15 18:06:07 +0800

    finish iteration F1: moving the cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,8 @@
 class StoreController < ApplicationController
-  include AccessCounter
+  include AccessCounter, CurrentCart
 
   before_action :increase_times, only: [:index]
+  before_action :set_start
 
   def index
     @products = Product.order(:title)

d283b40006987988f357e04e89762f7115354970
Cloneable <cloneable@163.com>
2020-12-18 22:02:50 +0800

    Iteration G1: Capture an Order Iteration G2: Atom Feed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,6 +61,16 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+    @product = Product.find(params[:id])
+    @last_order = @product.orders.order(:updated_at).last
+    if stale?(@last_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product

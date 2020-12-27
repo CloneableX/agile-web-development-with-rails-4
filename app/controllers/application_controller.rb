4069b1433f62f8aa6f636f08a6533f4a310bb12d
Cloneable <cloneable@163.com>
2020-12-27 14:13:35 +0800

    Iteration J1: Selecting the Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,31 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authorize
+  before_action :set_i18n_locale_from_params
 
   protected
 
     def authorize
       unless authorize_non_html or User.find_by(id: session[:user_id])
         redirect_to login_url, notice: 'Please log in.'
-      end   
+      end
+    end
+
+    def set_i18n_locale_from_params
+      unless params[:locale]
+        return
+      end
+
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash[:notice]
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
     end
 
   private
